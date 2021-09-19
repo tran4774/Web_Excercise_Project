6c8f5160594f11fdc8befc14d1c9e90603fd3366
Tran Nguyen <tran4774@gmail.com>
2021-09-19 15:28:18 +0700

    Downgrade Tomcat version from 10.0.10 to 9.0.27

--- src/main/java/com/webapp/webtest/web_project/HelloServlet.java
+++ src/main/java/com/webapp/webtest/web_project/HelloServlet.java
@@ -1,12 +1,12 @@
 package com.webapp.webtest.web_project;
 
 import java.io.*;
-import jakarta.servlet.RequestDispatcher;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/hello-servlet")
 public class HelloServlet extends HttpServlet {
