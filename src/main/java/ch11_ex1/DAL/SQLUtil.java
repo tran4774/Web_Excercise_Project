b421761dd344bf1353bf5c29d854b5891dd93627
Tran Nguyen <tran4774@gmail.com>
2021-10-29 20:43:51 +0700

    Add exercise ch11_ex1

---
+++ src/main/java/ch11_ex1/DAL/SQLUtil.java
@@ -0,0 +1,38 @@
+package ch11_ex1.DAL;
+
+import java.sql.*;
+
+public class SQLUtil
+{
+    public static String getHtmlTable(ResultSet results)
+            throws SQLException
+    {
+        StringBuilder htmlTable = new StringBuilder();
+        ResultSetMetaData metaData = results.getMetaData();
+        int columnCount = metaData.getColumnCount();
+
+        htmlTable.append("<table>");
+
+        htmlTable.append("<tr>");
+        for (int i = 1; i <= columnCount; i++) {
+            htmlTable.append("<th>");
+            htmlTable.append(metaData.getColumnName(i));
+            htmlTable.append("</th>");
+        }
+        htmlTable.append("</tr>");
+
+        while (results.next())
+        {
+            htmlTable.append("<tr>");
+            for (int i = 1; i <= columnCount; i++) {
+                htmlTable.append("<td>");
+                htmlTable.append(results.getString(i));
+                htmlTable.append("</td>");
+            }
+            htmlTable.append("</tr>");
+        }
+        
+        htmlTable.append("</table>");
+        return htmlTable.toString();
+    }    
+}
